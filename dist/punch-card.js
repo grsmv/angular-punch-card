3832f051c23c97e4e2cdb204b902ea1c56fc8320
Sergey Gerasimov <mail@grsmv.com>
2014-09-17 19:32:31 +0300

    Refreshing punch card after data change

--- dist/punch-card.js
+++ dist/punch-card.js
@@ -1,7 +1,11 @@
 (function() {
+  var punchCardTemplate;
+
   angular.module("punchCard", []);
 
-  angular.module("punchCard").directive("punchCard", function() {
+  punchCardTemplate = "<div id=\"punch-card\">\n    <div class=\"punch-card-day\" ng-repeat='day in days'>\n        <div class=\"punch-card-day-name\">\n            <div class=\"punch-card-day-name-label\">{{ day }}</div>\n        </div>\n        <div class=\"punch-card-hour\"\n             ng-repeat='hour in hours'\n             ng-init=\"n = data[$parent.$index][$index]\">\n            <div class=\"punch-card-hour-data size-{{ size(n) }}\"></div>\n            <div class=\"punch-card-hour-tooltip\" ng-show=\"n\">\n                <b>{{ n }}</b> {{ description(n) }}\n                <div class=\"arrow\"></div>\n            </div>\n            <div class=\"punch-card-hour-tick\"></div>\n        </div>\n    </div>\n    <div class=\"punch-card-hour-name\">\n        <div class=\"punch-card-hour-name-label\" ng-repeat='hour in hours'>\n            {{ hour }}\n        </div>\n    </div>\n</div>";
+
+  angular.module("punchCard").directive("punchCard", function($compile) {
     return {
       restrict: "AE",
       scope: {
@@ -9,26 +13,30 @@
         plural: "@",
         singular: "@"
       },
-      link: function($scope) {
-        var flatten, max;
+      template: punchCardTemplate,
+      link: function($scope, element) {
         $scope.days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         $scope.hours = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"];
-        flatten = [].concat.apply([], $scope.data);
-        max = flatten.sort(function(a, b) {
-          return a - b;
-        })[flatten.length - 1];
-        $scope.size = function(n) {
-          return Math.floor(100 / max * +n / 10);
-        };
-        return $scope.description = function(n) {
+        $scope.description = function(n) {
           if (n === 1) {
             return $scope.singular || "event";
           } else {
             return $scope.plural || "events";
           }
         };
-      },
-      template: "<div id=\"punch-card\">\n    <div class=\"punch-card-day\" ng-repeat='day in days'>\n        <div class=\"punch-card-day-name\">\n            <div class=\"punch-card-day-name-label\">{{ day }}</div>\n        </div>\n        <div class=\"punch-card-hour\"\n             ng-repeat='hour in hours'\n             ng-init=\"n = data[$parent.$index][$index]\">\n            <div class=\"punch-card-hour-data size-{{ size(n) }}\"></div>\n            <div class=\"punch-card-hour-tooltip\" ng-show=\"n\">\n                <b>{{ n }}</b> {{ description(n) }}\n                <div class=\"arrow\"></div>\n            </div>\n            <div class=\"punch-card-hour-tick\"></div>\n        </div>\n    </div>\n    <div class=\"punch-card-hour-name\">\n        <div class=\"punch-card-hour-name-label\" ng-repeat='hour in hours'>\n            {{ hour }}\n        </div>\n    </div>\n</div>"
+        return $scope.$watch("data", function(data) {
+          var flatten, max;
+          flatten = [].concat.apply([], $scope.data);
+          max = flatten.sort(function(a, b) {
+            return a - b;
+          })[flatten.length - 1];
+          $scope.size = function(n) {
+            return Math.floor(100 / max * +n / 10);
+          };
+          element.empty();
+          return element.append($compile(punchCardTemplate)($scope));
+        });
+      }
     };
   });
 
--- src/punch-card.coffee
+++ src/punch-card.coffee
@@ -19,7 +19,33 @@
 
 angular.module "punchCard", []
 
-angular.module("punchCard").directive "punchCard" , ->
+punchCardTemplate = """
+    <div id="punch-card">
+        <div class="punch-card-day" ng-repeat='day in days'>
+            <div class="punch-card-day-name">
+                <div class="punch-card-day-name-label">{{ day }}</div>
+            </div>
+            <div class="punch-card-hour"
+                 ng-repeat='hour in hours'
+                 ng-init="n = data[$parent.$index][$index]">
+                <div class="punch-card-hour-data size-{{ size(n) }}"></div>
+                <div class="punch-card-hour-tooltip" ng-show="n">
+                    <b>{{ n }}</b> {{ description(n) }}
+                    <div class="arrow"></div>
+                </div>
+                <div class="punch-card-hour-tick"></div>
+            </div>
+        </div>
+        <div class="punch-card-hour-name">
+            <div class="punch-card-hour-name-label" ng-repeat='hour in hours'>
+                {{ hour }}
+            </div>
+        </div>
+    </div>
+"""
+
+
+angular.module("punchCard").directive "punchCard" , ($compile) ->
     restrict: "AE"
 
     scope:
@@ -27,7 +53,9 @@ angular.module("punchCard").directive "punchCard" , ->
         plural: "@"
         singular: "@"
 
-    link: ($scope) ->
+    template: punchCardTemplate
+
+    link: ($scope, element) ->
         $scope.days = [
             "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
         ]
@@ -35,38 +63,19 @@ angular.module("punchCard").directive "punchCard" , ->
             "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11",
             "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"
         ]
-
-        flatten = [].concat.apply([], $scope.data)
-        max = flatten.sort((a, b) -> a - b)[flatten.length - 1]
-        $scope.size = (n) -> Math.floor(100 / max * +n / 10)
-
         $scope.description = (n) ->
             if n == 1
                 $scope.singular || "event"
             else
                 $scope.plural || "events"
 
-    template: """
-        <div id="punch-card">
-            <div class="punch-card-day" ng-repeat='day in days'>
-                <div class="punch-card-day-name">
-                    <div class="punch-card-day-name-label">{{ day }}</div>
-                </div>
-                <div class="punch-card-hour"
-                     ng-repeat='hour in hours'
-                     ng-init="n = data[$parent.$index][$index]">
-                    <div class="punch-card-hour-data size-{{ size(n) }}"></div>
-                    <div class="punch-card-hour-tooltip" ng-show="n">
-                        <b>{{ n }}</b> {{ description(n) }}
-                        <div class="arrow"></div>
-                    </div>
-                    <div class="punch-card-hour-tick"></div>
-                </div>
-            </div>
-            <div class="punch-card-hour-name">
-                <div class="punch-card-hour-name-label" ng-repeat='hour in hours'>
-                    {{ hour }}
-                </div>
-            </div>
-        </div>
-    """
+        $scope.$watch "data", (data) ->
+
+            # recalculating sizes
+            flatten = [].concat.apply([], $scope.data)
+            max = flatten.sort((a, b) -> a - b)[flatten.length - 1]
+            $scope.size = (n) -> Math.floor(100 / max * +n / 10)
+
+            element.empty()
+            element.append $compile(punchCardTemplate)($scope)
+
